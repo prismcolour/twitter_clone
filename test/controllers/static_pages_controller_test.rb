149bea0b227d01f9fc2865ef760e713bee229de8
prismcolour <prismcolour@icloud.com>
2017-05-06 15:20:05 +0000

    Add test unit for root in test static pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ require 'test_helper'
 # end
   
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
