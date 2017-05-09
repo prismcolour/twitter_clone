4bb884cc4911d633064b3768a2d8b9ee6df72f8c
prismcolour <prismcolour@icloud.com>
2017-05-09 23:30:26 +0000

    Add signup page to integration test to check title

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -6,5 +6,6 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
     assert_equal full_title("About"), "About | Ruby on Rails Tutorial Sample App"
     assert_equal full_title("Contact"), "Contact | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Signup"), "Signup | Ruby on Rails Tutorial Sample App"
   end
 end
