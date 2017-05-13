5b6c693c27a6c0253ee1ce858b4d36ba9f3da3dc
prismcolour <prismcolour@icloud.com>
2017-05-13 14:24:37 +0000

    Add create action in user controller to process form and add auxiliary method for strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+  end
+
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
