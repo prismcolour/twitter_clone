fc426722e3a30ee230eb2d9811c5173c649b3c65
prismcolour <prismcolour@icloud.com>
2017-05-16 19:34:28 +0000

    Add user digest method in user model to be used to generate password for fixture in test suite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  # Returns the hash digest of the given string/
+  # Used in the test fixtures file to generate the hash password 
+  # User.digest
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
