d307342e95a145b45871822b1ebae7e7980a85c3
prismcolour <prismcolour@icloud.com>
2017-05-16 19:59:36 +0000

    Add unit test for login with valid information followed by logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,4 +15,30 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+  
+  test "login with valid information followed by logout" do
+    # What the login process should look like
+    get login_path
+    post login_path, params: { session: { email:  @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    # Check to make sure right page is being displayed upon login
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    
+    # Logout testing
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    # Check to make sure right page is being displayed upon logout
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
 end
