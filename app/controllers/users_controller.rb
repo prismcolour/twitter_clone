42bb8b9fdd00e8337daa0d6f3e0f200afe05affa
prismcolour <prismcolour@icloud.com>
2017-05-21 16:56:23 +0000

    Add store_location in user controller when user is not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,8 @@ def update
   # User must be logged in
     def logged_in_user
       unless logged_in?
+        # Stores location for friendly redirect when user not logged in
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
