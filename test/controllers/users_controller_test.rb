1056b506bec378ed76d8c07bca20be955f2c2231
prismcolour <prismcolour@icloud.com>
2017-05-21 15:58:24 +0000

    Add test unit for trying to edit as a wrong user - fixes security breach

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,17 +11,38 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
-
+  
+  # Design pattern to test edits/updates made when user is not logged in 
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
+  # Design pattern to test edits/updates made when user is not logged in 
   test "should redirect update when not logged in" do
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+  
+  # Design pattern to test edits/updates made when user is the wrong user
+  # Redirect to root because user is logged in already
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
+  # Design pattern to test edits/updates made when user is the wrong user
+  # Redirect to root because user is logged in already 
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
