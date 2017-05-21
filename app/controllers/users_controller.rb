a9b670501052a878fcb2207889d61c017fe1e671
prismcolour <prismcolour@icloud.com>
2017-05-21 14:49:49 +0000

    Add boolean logged_in_user method in private section of users controller and add before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -39,4 +41,17 @@ def update
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+  # Before filters
+  # Confirms a logged-in user.
+  # Method to fix security breaches for access to pages
+  # User must be logged in
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
+
+
