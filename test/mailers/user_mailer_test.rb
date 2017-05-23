a4348a9dd684c805cade958440705637d44856cb
prismcolour <prismcolour@icloud.com>
2017-05-23 20:46:05 +0000

    Update config file environment with correct host and clear out auto-generated mail.from values -- test suite green at this commit

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,18 +3,28 @@ require 'test_helper'
 class UserMailerTest < ActionMailer::TestCase
 
   test "account_activation" do
-    mail = UserMailer.account_activation
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name,               mail.body.encoded
+    assert_match user.activation_token,   mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 
   test "password_reset" do
     mail = UserMailer.password_reset
     assert_equal "Password reset", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 end
+
+# Had to update the config file for test environment for this test
+# to pass.  assert_equal is default from: 'noreply@example.com' from
+# the application mailer base class default setting. 
+# Rails auto-generated "from@example.com" causing assert_equal
+# fail
