35c91d6ada9df6441d936ce8f1e70dc3b27b658b
prismcolour <prismcolour@icloud.com>
2017-05-23 20:01:19 +0000

    Add a working preview method for account activation in test mailer preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,11 +2,24 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # This should be previewing at http://michael-hartl-rails-tutorial-prismcolour.c9users.io and not
+  # localhost:3000 because the host config was updated in 
+  # config/environments/development.rb
   def account_activation
-    UserMailer.account_activation
+    # UserMailer.account_activation 
+    
+    # Updated code becaus account_activation method
+    # needs a valid user
+    # user variable equal to the first user in the development database
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # This should be previewing at http://michael-hartl-rails-tutorial-prismcolour.c9users.io and not
+  # localhost:3000 because the host config was updated in 
+  # config/environments/development.rb
   def password_reset
     UserMailer.password_reset
   end
