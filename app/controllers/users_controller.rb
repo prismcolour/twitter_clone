fab0d28072a7b99e43e5bbd6c38ff082b639b1cf
prismcolour <prismcolour@icloud.com>
2017-05-24 17:33:32 +0000

    Refactor edit action in account activations controller to use .activate method from user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
