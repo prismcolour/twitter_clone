5c2d824b794268c056c78035216490516d8d106b
prismcolour <prismcolour@icloud.com>
2017-05-24 17:11:44 +0000

    Update create action in sessions controller to prevent unactivated users from logging in - test suite green

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
       # Log the user in and redirect to the user's show page.
       log_in @user
       # remember user - ORIGINAL CODE/method call 
@@ -16,6 +17,14 @@ class SessionsController < ApplicationController
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       # redirect_to @user OLD CODE - updated with friendly forwarding
       redirect_back_or @user
+      # Add on else block 
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
+      
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
