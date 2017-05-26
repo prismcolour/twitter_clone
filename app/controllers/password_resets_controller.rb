940bcf17da2a17c5e536acf6337ead8a4c403183
prismcolour <prismcolour@icloud.com>
2017-05-26 00:26:31 +0000

    Add strong parameters for reset password form - security measure

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -25,6 +25,10 @@ class PasswordResetsController < ApplicationController
   
    private
 
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+    
     def get_user
       @user = User.find_by(email: params[:email])
     end
