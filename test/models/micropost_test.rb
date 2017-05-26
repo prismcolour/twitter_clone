6284bf6f80879e18080bc7550b67b1e79ff4bb05
prismcolour <prismcolour@icloud.com>
2017-05-26 19:45:55 +0000

    Add unit tests for validations in micropost model

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,4 +4,23 @@ class MicropostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @user = users(:michael)
+    # Idiomatically correct 
+    # Creating the micropost object via association
+    # This will auto-load the user_id when building the object
+    # this way instead of Micropost.new
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+  
 end
