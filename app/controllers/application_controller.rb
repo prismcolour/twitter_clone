d312a155ddb911cc0e636a7f2eeaaec97f7ccf4e
prismcolour <prismcolour@icloud.com>
2017-05-28 16:03:48 +0000

    Refactor logged_in_user private method - move to applications_controller for inheritance in controller class - check login for microposts and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  private
+  
+  # Before filters
+  # Confirms a logged-in user.
+  # Method to fix security breaches for access to pages
+  # User must be logged in
+    def logged_in_user
+      unless logged_in?
+        # Stores location for friendly redirect when user not logged in
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,19 +54,6 @@ def update
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
-  # Before filters
-  # Confirms a logged-in user.
-  # Method to fix security breaches for access to pages
-  # User must be logged in
-    def logged_in_user
-      unless logged_in?
-        # Stores location for friendly redirect when user not logged in
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
     # Confirms the correct user.
     # Fix for security breach when a current user is trying to make edits/updates to 
     # another user's profile
