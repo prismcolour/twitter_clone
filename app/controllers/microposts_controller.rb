621f7ad5fa52a7467b3a3151eaa2e5fa069fe780
prismcolour <prismcolour@icloud.com>
2017-05-28 16:08:05 +0000

    Add strong parameters for micropost content - security

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,4 +8,10 @@ class MicropostsController < ApplicationController
   def destroy
   end
   
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+    
 end
