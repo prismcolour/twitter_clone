f687ca78576ef1eba9caa8a35fa93f909e0a9571
prismcolour <prismcolour@icloud.com>
2017-06-01 15:19:44 +0000

    Add feed method in user model - use key value pairs in active record where method

--- app/models/user.rb
+++ app/models/user.rb
@@ -134,6 +134,12 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
   
+   # Returns a user's status feed.
+  def feed
+    Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+                    following_ids: following_ids, user_id: id)
+  end
+  
   private
 
     # Converts email to all lower-case.
